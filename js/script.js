aacab3e4029b3348d2d6cdd35b4fcfea9e17c88b
naksmedia <43466177+naksmedia@users.noreply.github.com>
2019-02-07 17:27:30 +0300

    Верстк от 7 января (контакты, новости, о нас) правки: футер, форма отправить заявку, хедр

--- js/script.js
+++ js/script.js
@@ -1,48 +1,14 @@
 // Кнопка поиска
 window.onload = function () {
-  $(".btn").click(function () {
-    $('.search, btn').toggleClass('open');
-  });
+    // Обрез текста
+    var size = 175,
+    newsContent= $('.box__news__text'),
+    newsText = newsContent.text();
+    
+if(newsText.length > size){
+  newsContent.text(newsText.slice(0, size) + '...');
+}
 
-  //  
-
-  $(document).ready(function () {
-    $('#pop-up').click(function () {
-      $('#modal-window').addClass('active');
-    });
-    $('.modal-close').click(function () {
-      $(this).parent().removeClass('active');
-    });
-  });
-
-
-  function check() {
-    var submit = document.getElementsByName('submit')[0];
-    if (document.getElementById('politics').checked)
-      submit.disabled = '';
-    else
-      submit.disabled = 'disabled';
-  }
-
-  $('#box').click(function () {
-    $('.drop__menu').hide(); // Скрывает начальное содержимое.
-    $('.drop__menu').show(); // Показывает содержимое диалога.
-  });
-
-  $(document).ready(function () {
-    $(".sb-icon-search").click(function () {
-      if (!$(".sb-search").hasClass("sb-search-open")) {
-        $(".sb-search").addClass("sb-search-open");
-      }
-    });
-
-    $(".sb-search-submit").click(function () {
-      if ($(".sb-search").hasClass("sb-search-open") && $.trim($(".sb-search-input").val()).length == 0) {
-        event.preventDefault();
-        $(".sb-search").removeClass("sb-search-open");
-      }
-    });
-  });
 
 
   // Бургер меню
@@ -216,14 +182,6 @@ window.onload = function () {
       MessageAreaText: "Прикрепить файлы"
     });
   });
-
-
+  
 
 };
-
-  // require(["popper"], function(popper) {
-  //   window.Popper = popper;
-  //   require(["bootstrap"]);
-  // });
-  // Ввод номера - маска
-
